666fc99d28ac3bf55553083c38554313fc95880a
Sasiru-Jayawardhana <sasiru.20210801@iit.ac.lk>
2025-03-29 12:29:22 +0530

    merging project into existing repo

---
+++ YTcloud.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Navigate to the directory containing GUI.py
+cd /d "C:\YT downloader"
+
+:: Run the GUI.py file using Python
+python GUI.py
+
+:: Pause the terminal to display any errors or output
+pauses
